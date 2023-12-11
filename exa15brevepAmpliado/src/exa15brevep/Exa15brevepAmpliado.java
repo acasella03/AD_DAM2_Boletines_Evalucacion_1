ac30a64353a58f2a72e68c7807055eebe3552832
acasella03 <114586433+acasella03@users.noreply.github.com>
2023-12-11 20:18:30 +0100

    Modificación

--- exa15brevepAmpliado/src/exa15brevep/Exa15brevepAmpliado.java
+++ exa15brevepAmpliado/src/exa15brevep/Exa15brevepAmpliado.java
@@ -1,96 +1,18 @@
 package exa15brevep;
 
-import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 public class Exa15brevepAmpliado {
 
-    public static void main(String[] args) {
-        Connection conn = null;
-        try {
-            conn = conexion();
-
-             // Consulta para obtener los datos de composicion para p1 y p2
-           String sql = "SELECT c.codc, c.nomec, c.graxa, co.peso, co.codp " +
-                         "FROM composicion co " +
-                         "INNER JOIN componentes c ON co.codc = c.codc " +
-                         "WHERE co.codp IN ('p1','p2')";
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            Map<String, Integer> grasaPorPlato = new HashMap<>();
-
-            // Calcular la grasa parcial por componente y plato
-            while (resultSet.next()) {
-                String codc = resultSet.getString("codc");
-                int graxa = resultSet.getInt("graxa");
-                int peso = resultSet.getInt("peso");
-                String codp = resultSet.getString("codp");
-                int grasaParcial = (peso * graxa) / 100;
-
-                // Agregar la grasa parcial al plato correspondiente
-                grasaPorPlato.putIfAbsent(codp, 0);
-                grasaPorPlato.put(codp, grasaPorPlato.get(codp) + grasaParcial);
-            }
-
-            // Paso 2: Leer el archivo serializado
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("platoss"))) {
-                Platos plato;
-                //Crear un archivo XML para escribir los datos
-                XMLOutputFactory xmlOutputFactory=XMLOutputFactory.newInstance();
-                XMLStreamWriter writer= xmlOutputFactory.createXMLStreamWriter(new FileWriter("totalgraxas.xml"));
-                
-                writer.writeStartDocument();
-                writer.writeStartElement("Platos");
-                
-                while ((plato = (Platos) ois.readObject()) != null) {
-                    String codp = plato.getCodigop();
-                    // Paso 4: Imprimir el código, nombre del plato y grasa total
-                    if (grasaPorPlato.containsKey(codp)) {
-                        writer.writeStartElement("Plato");
-                        writer.writeAttribute("Codigop", codp);
-                        
-                        writer.writeStartElement("nomep");
-                        writer.writeCharacters(plato.getNomep());
-                        writer.writeEndElement();
-                        
-                        writer.writeStartElement("graxatotal");
-                        writer.writeCharacters(Integer.toString(grasaPorPlato.get(codp)));
-                        writer.writeEndElement();
-                        
-                        writer.writeEndElement();
-                    }
-                }
-                writer.writeEndElement();
-                writer.writeEndDocument();
-                writer.close();
-            } catch (EOFException e) {
-                // Fin del archivo
-            }
-
-        } catch (SQLException | IOException | ClassNotFoundException | XMLStreamException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static Connection conexion() throws SQLException {
+    private static Connection conexion() throws SQLException {
         String driver = "jdbc:postgresql:";
         String host = "//localhost:";
         String porto = "5432";
@@ -101,13 +23,80 @@ public class Exa15brevepAmpliado {
         return DriverManager.getConnection(url, usuario, password);
     }
 
-    public static void cerrarConexion(Connection conn) {
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
+    public static void main(String[] args) throws SQLException, FileNotFoundException, IOException, ClassNotFoundException, XMLStreamException {
+        String nombreArchivoSerializado = "platoss";
+
+        Connection conn = conexion();
+
+        // Leer objetos desde el archivo
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nombreArchivoSerializado));
+        // Objeto para almacenar el objeto leído
+        Platos objetoLeido;
+        //Crear un archivo XML para escribir los datos
+        XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newInstance();
+        XMLStreamWriter writer = xmlOutputFactory.createXMLStreamWriter(new FileWriter("totalgraxas.xml"));
+        writer.writeStartDocument();
+        writer.writeStartElement("Platos");
+        // Bucle mientras se pueda leer un objeto no nulo
+        while ((objetoLeido = (Platos) ois.readObject()) != null) {
+            Platos plato = objetoLeido;
+            // Imprimir los valores individualmente por consola
+            String codp = plato.getCodigop();
+            String nombrePlato = plato.getNomep();
+            System.out.println(codp + "\n" + nombrePlato);
+
+            //Escribir en el XML
+            writer.writeStartElement("Plato");
+            writer.writeAttribute("Codigop", codp);
+
+            writer.writeStartElement("nomep");
+            writer.writeCharacters(nombrePlato);
+            writer.writeEndElement();
+
+            String codc;
+            int peso = 0;
+            int grasa = 0;
+            int grasaParcial = 0;
+            int grasaTotal = 0;
+            //Consultar el peso del plato y codigo del componente
+            PreparedStatement obtenerPesoPstmt = conn.prepareStatement("SELECT * FROM composicion where codp=?");
+            obtenerPesoPstmt.setString(1, codp);
+            ResultSet rs1 = obtenerPesoPstmt.executeQuery();
+            while (rs1.next()) {
+                codc = rs1.getString("codc");
+                peso = rs1.getInt("peso");
+
+                //Consultar grasa
+                PreparedStatement obtenerGrasaPstmt = conn.prepareStatement("SELECT graxa FROM componentes where codc=?");
+                obtenerGrasaPstmt.setString(1, codc);
+                ResultSet rs2 = obtenerGrasaPstmt.executeQuery();
+                rs2.next();
+                grasa = rs2.getInt("graxa");
+                obtenerGrasaPstmt.close();
+                rs2.close();
+
+                grasaParcial = peso / 100 * grasa;
+                grasaTotal = grasaTotal + grasaParcial;
+
             }
+            System.out.println("grasaTotal: " + grasaTotal + "\n"); //Imprimir por consola
+            
+            //Escribir XML
+            writer.writeStartElement("graxatotal");
+            writer.writeCharacters(String.valueOf(grasaTotal));
+            writer.writeEndElement();
+
+            writer.writeEndElement();
+
+            obtenerPesoPstmt.close();
+            rs1.close();
+
         }
+        writer.writeEndElement();
+        writer.writeEndDocument();
+        writer.close();
+        System.out.println("Fin del archivo.");
+        ois.close();
+        conn.close();
     }
 }
